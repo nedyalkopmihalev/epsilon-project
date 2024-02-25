58f9e6427c3a250bbee74b15413c4b8232bb8238
nedyalkopmihalev <nedialko81@abv.bg>
2024-02-25 20:19:52 +0200

    initial commit

---
+++ app/Http/Controllers/OAuth/OAuth.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\OAuth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class OAuth extends Controller
+{
+
+}
